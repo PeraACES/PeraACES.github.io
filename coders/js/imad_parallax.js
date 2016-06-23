d53d8f19d99ba9e17c563c368a66f33656b104ee
imadx <ahtimadhusanka@gmail.com>
2016-06-23 22:25:20 +0530

    added sponsors + updated parallax

--- coders/js/imad_parallax.js
+++ coders/js/imad_parallax.js
@@ -9,9 +9,11 @@ $(document).bind("mousemove", function(e){
 	var motionSpan_0 = ((i_width/2) - e.pageX)/i_width * depth;
 	var motionSpan_1 = ((i_width/2) - e.pageX)/i_width * depth * 0.6;
 	var motionSpan_2 = ((i_width/2) - e.pageX)/i_width * depth * 0.2;
+	var motionSpan_1_y = (e.pageY)/i_width * depth * 0.6;
+	var motionSpan_2_y = (e.pageY)/i_width * depth * 0.2;
 	bg_0.css("transform", "translateX(" + motionSpan_0 + "px)");
-	bg_1.css("transform", "translateX(" + motionSpan_1 + "px)");
-	bg_2.css("transform", "translateX(" + motionSpan_2 + "px)");
+	bg_1.css("transform", "translate(" + motionSpan_1 + "px, "+ (motionSpan_2_y + 5) + "px)");
+	bg_2.css("transform", "translate(" + motionSpan_2 + "px, "+ (motionSpan_1_y + 5) + "px)");
 });
 $(window).resize(function(){
 	i_width = $(window).width();
